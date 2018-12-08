cf742a3eb270112acc60965ec094c76b71d3585d
prabhu-durasoft <iamprabhu@yahoo.com>
2018-12-08 13:12:10 +0530

    Day03

--- Day03Client/src/main/java/com/synechron/CityInfoController.java
+++ Day03Client/src/main/java/com/synechron/CityInfoController.java
@@ -1,11 +1,17 @@
 package com.synechron;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 public class CityInfoController {
@@ -14,11 +20,42 @@ public class CityInfoController {
 	private String weatherServiceBaseUrl;
 	
 	private RestTemplate restTemplate = new RestTemplate();
+	private Map<String, String> cache = new HashMap<>();
 	
 	@GetMapping("/weatherinfo/{city}")
+	@HystrixCommand(fallbackMethod="getWeatherInfoFromCache", commandProperties = {
+			@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", 
+					value="5000")	
+	})
 	public String getWeatherInfoFromServer(@PathVariable String city) {
+		System.out.println("*******calling getWeatherInfoFromServer");
 		String url = weatherServiceBaseUrl + "/" + city;
 		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-		return responseEntity.getBody();
+		String output = responseEntity.getBody();
+		cache.put(city, output);
+		return output;
 	}
+	
+	public String getWeatherInfoFromCache(String city) {
+		System.out.println("-----calling getWeatherInfoFromCache");
+		String message = cache.get(city);
+		if(message == null) {
+			message = "Oops! WeatherService may be down temporarily";
+		}
+		else {
+			message = "From Cache: " + message;
+		}
+		
+		return message;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Day03Client/src/main/java/com/synechron/Day03ClientApplication.java
+++ Day03Client/src/main/java/com/synechron/Day03ClientApplication.java
@@ -2,8 +2,10 @@ package com.synechron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class Day03ClientApplication {
 
 	public static void main(String[] args) {
